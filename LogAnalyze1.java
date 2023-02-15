2953d7212b5a6838de0da3a3865bbfc9dd8452d9
0T-T0 <97810103+0T-T0@users.noreply.github.com>
2023-02-15 17:04:16 -0500

    Add files via upload

---
+++ LogAnalyze1.java
@@ -0,0 +1,61 @@
+package com.example;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class LogAnalyze1 {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "LogAnalyze1");
+        job.setJarByClass(LogAnalyze1.class);
+        job.setMapperClass(LogAnalyze1.accessLogMap.class);
+        job.setReducerClass(LogAnalyze1.accessLogReduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputKeyClass(Text.class);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+
+    public static class accessLogMap extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable valueOne = new IntWritable(1);
+        private final static String URLtoMatch = "/assets/img/home-logo.png";
+        private final static Text matchPattern = new Text(URLtoMatch);
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String textValue = value.toString();
+            if(textValue.contains(URLtoMatch)) context.write(matchPattern,valueOne);
+        }
+    }
+
+    public static class accessLogReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable i : values) sum += i.get();
+            context.write(key, new IntWritable(sum));
+        }
+    }
+}
