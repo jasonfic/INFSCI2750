02befd9663e4eeb750256d009b6dde377b4177cc
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-02-16 18:07:52 -0500

    Handle Input Above the Maximum Size

--- MiniProject1/src/main/java/group4/MiniProject1/HadoopNGramFrequency.java
+++ MiniProject1/src/main/java/group4/MiniProject1/HadoopNGramFrequency.java
@@ -1,6 +1,8 @@
 package group4.MiniProject1;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -20,6 +22,7 @@ public class HadoopNGramFrequency {
 	// https://hadoop.apache.org/docs/current/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
 	
 	private static int n = 1;
+	private static int maxBytes = 5000;
 
 	public static class NGramMapper extends Mapper<Object, Text, Text, IntWritable>{
 
@@ -29,13 +32,23 @@ public class HadoopNGramFrequency {
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			Configuration conf = context.getConfiguration();
 	    	int n = conf.getInt("n", 1);
+	    	int maxBytes = conf.getInt("maxBytes", 5000);
 	    	System.out.print("Mapper value for n: ");
 	    	System.out.println(n);
+	    	System.out.print("Mapper max byte value: ");
+	    	System.out.println(maxBytes);
 	    	
 	        String input = value.toString();
 	        input = input.replaceAll("\\s+", "");
-	        List<String> joined = new ArrayList<String>();
 	        
+	        // Adapted from https://stackoverflow.com/questions/119328/how-do-i-truncate-a-java-string-to-fit-in-a-given-number-of-bytes-once-utf-8-en
+	        Charset charset = Charset.forName("UTF-8");
+	        byte[] byteSize = input.getBytes(charset);
+	        if (byteSize.length >= maxBytes) {
+	            input = new String(input.getBytes("UTF-8"), 0, maxBytes, "UTF-8");
+	        }
+	        
+	        List<String> joined = new ArrayList<String>();
 	        // Split input text with a space as a delimiter every n characters
 			for (int i = 0; i < input.length()-(n-1); i++) {
 				String index = input.substring(i, i+n);
@@ -67,15 +80,21 @@ public class HadoopNGramFrequency {
 	
 	public static void main(String[] args) throws Exception {
 		
-		// Collect n value from command line input while avoiding out of bounds exceptions
+		// Collect n and max byte size values from command line input while avoiding out of bounds exceptions
 		if (args.length > 2) {
 			n = Integer.parseInt(args[2]);
+			if (args.length > 3) {
+				maxBytes = Integer.parseInt(args[3]);
+			}
 		}
 		System.out.print("Text split into chunks of size: ");
 		System.out.println(n);
+		System.out.print("Maximum input byte size: ");
+		System.out.println(maxBytes);
 		
 		Configuration conf = new Configuration();
 		conf.setInt("n", n);
+		conf.setInt("maxBytes", maxBytes);
 		Job job = Job.getInstance(conf, "ngram frequency counter");
 		job.setJarByClass(HadoopNGramFrequency.class);
 		job.setMapperClass(NGramMapper.class);
