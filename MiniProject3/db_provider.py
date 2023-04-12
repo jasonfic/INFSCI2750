f83dd4ede1d584c47f29d87f77774f1814d60be5
jasonfic <43254577+jasonfic@users.noreply.github.com>
2023-04-11 22:10:41 -0400

    Add Quotes Around Query Input

--- MiniProject3/db_provider.py
+++ MiniProject3/db_provider.py
@@ -26,11 +26,11 @@ class Server:
         # query = "INSERT INTO " + self.table + "(key) VALUES (" + hash.hexdigest() + ");"
         # print(query)
         # self.session.execute(query)
-        query = "INSERT INTO " + self.table + "(key, value) VALUES (" + key + "," + value + ");"
+        query = "INSERT INTO " + self.table + "(key, value) VALUES ('" + key + "', '" + value + "');"
         print(query)
         self.session.execute(query)
 
 
     # Retrieve value by key
     def get_data(self, key):
-        self.session.execute("SELECT value FROM " + self.table + " WHERE key = " + key + ";")
+        self.session.execute("SELECT value FROM " + self.table + " WHERE key = '" + key + "';")
